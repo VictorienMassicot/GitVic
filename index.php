9c53758e26a1eeca427404cbbe5591eac911d166
VictorienMassicot <victorien.massicot@gmail.com>
2021-11-15 15:31:24 +0100

    modifs CSS

--- index.php
+++ index.php
@@ -12,32 +12,32 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="style.css"> 
 
     <title>Projet de Vic</title>
 </head>
 <body class="bg-dark">
-    <div class="container">
+    <div class="container d-flex flex-column">
         <h1 class="text-center text-light display-2 my-4">Mon projet</h1>
 
         <div id="div1">
-            <img src="img/sid.jpg" style="width:200px;" class="d-block mx-auto" alt="Moi">
-            <h2 class="text-white text-center mt-2">Biographie</h2>
-            <p class="text-white text-center m-2">Je m'appelle Victorien MASSICOT, j'ai 21 ans et je vis à Aubergenville. Je suis titulaire d'un baccalauréat professionnel en gestion-administration ainsi qu'un BTS gestion PME</p>
+            <img src="img/sid.jpg" class="d-block mx-auto" alt="Moi">
+            <h2 class="text-white text-center fs-1 mt-2">Biographie</h2>
+            <p id="paraph" class="text-white text-center fs-4 m-2">Je m'appelle Victorien MASSICOT, j'ai 21 ans et je vis à Aubergenville. Je suis titulaire d'un baccalauréat professionnel en gestion-administration ainsi qu'un BTS gestion PME</p>
         </div>
 
         <hr class="bg-white">
 
-        <h2 class="display-4 text-white">A PROPOS DE MOI</h2>
+        <h2 class="display-4 text-center text-white">A PROPOS DE MOI</h2>
 
         <div class="d-flex flex-row align-items-center">
             <div class="col-md-4">
                 <img src="img/gsw.png" alt="GSW">
             </div>
             <div class="col-md-8">
-                <p class="text-white" style="font-size:16px;">En NBA, je supporte les Golden State Warriors.<br> Mon joueur favori est Klay Thompson</p>
+                <p class="text-white fs-4">En NBA, je supporte les Golden State Warriors.<br> Mon joueur favori est Klay Thompson</p>
             </div>
         </div>
 
