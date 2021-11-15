585161b9435a910b3aa03361b1a8a522e0035d1b
VictorienMassicot <victorien.massicot@gmail.com>
2021-11-15 16:19:22 +0100

    modifs CSS + ajout dans A PROPOS

--- index.php
+++ index.php
@@ -19,28 +19,39 @@
     <title>Projet de Vic</title>
 </head>
 <body class="bg-dark">
-    <div class="container d-flex flex-column">
-        <h1 class="text-center text-light display-2 my-4">Mon projet</h1>
+    <div class="container d-flex flex-column justify-content-center">
+        
 
-        <div id="div1">
-            <img src="img/sid.jpg" class="d-block mx-auto" alt="Moi">
+        <div id="div1" class="d-flex flex-column justify-content-center">
+            <h1 class="text-center text-light display-2 my-4">Mon projet</h1>
+            <img src="img/sid.jpg" class="mx-auto imgBorder" alt="Moi">
             <h2 class="text-white text-center fs-1 mt-2">Biographie</h2>
             <p id="paraph" class="text-white text-center fs-4 m-2">Je m'appelle Victorien MASSICOT, j'ai 21 ans et je vis à Aubergenville. Je suis titulaire d'un baccalauréat professionnel en gestion-administration ainsi qu'un BTS gestion PME</p>
         </div>
 
         <hr class="bg-white">
 
-        <h2 class="display-4 text-center text-white">A PROPOS DE MOI</h2>
-
-        <div class="d-flex flex-row align-items-center">
-            <div class="col-md-4">
-                <img src="img/gsw.png" alt="GSW">
+        
+        <div id="div2" class="d-flex flex-column justify-content-center">
+            <h2 class="display-4 text-center text-white mt-2">A PROPOS DE MOI</h2>
+            <div class="d-flex flex-row align-items-center justify-content-evenly">
+                <div>
+                    <img src="img/gsw.png" class="imgBorder" alt="GSW">
+                </div>
+                <div>
+                    <p class="text-white fs-4">En NBA, je supporte les Golden State Warriors.<br> Mon joueur favori est Klay Thompson</p>
+                </div>
             </div>
-            <div class="col-md-8">
-                <p class="text-white fs-4">En NBA, je supporte les Golden State Warriors.<br> Mon joueur favori est Klay Thompson</p>
+
+            <div class="d-flex flex-row align-items-center justify-content-evenly">
+                <div>
+                    <p class="text-white fs-4">Pour le football, je supporte le PSG depuis tout petit.<br> Mon joueur préféré est Pauleta</p>
+                </div>
+                <div>
+                    <img src="img/psg.png" alt="PSG">
+                </div>
             </div>
         </div>
-
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
